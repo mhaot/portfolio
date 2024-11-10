4a807ab17f3ae20b20004a3f61c298a40777d46b
‘aota201507071020’ <aota20150707.1020@gmail.com>
2024-11-11 06:21:43 +0900

    11/10

---
+++ assets/js/apps.js
@@ -0,0 +1,25 @@
+// // apps.jsに記述してHTMLのbodyの閉じタグ前に<script type="module" src="assets/js/apps.js"></script>
+
+// import { initCarousel } from './carousel.js'; // {関数名}from'ファイル名';
+// initCarousel();// 呼び出し';
+
+// import { initHeader } from './header.js';
+// initHeader();
+
+// import { initMain } from './main.js';
+// initMain();
+
+// import { initFooter } from './footer.js';
+// initFooter();
+
+// // ↓増やしたファイルをその都度記述
+
+
+// // carousel.js 各ファイルの上に記述
+// export function initCarousel() {
+//     // carouselの初期化処理
+// }
+// export function initCarousel() {
+//     const container = document.querySelector('.carousel-container'); // HTMLのクラス名を指定
+//     // 初期化や操作のコードをここに記述
+//     }
