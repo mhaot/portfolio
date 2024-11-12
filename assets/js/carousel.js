696cef37c130283cf50415c0d7870276be5d2d64
‘aota201507071020’ <aota20150707.1020@gmail.com>
2024-11-13 06:40:27 +0900

    11/12

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -7,30 +7,23 @@ export function initCarousel() {
 //     });
 //     }
 
-let currentIndex = 0; // 初期インデックスを設定、最初に表示するスライドのインデックス(0番目のスライド)
-const slides = document.querySelectorAll('.l-carousel__slides--item'); //スライドの要素を全て取得
-const indicators = document.querySelectorAll('.c-btn__indicator'); //インジケーターの要素を全て取得
+let currentIndex = 0; // 初期インデックス設定
+const slides = document.querySelectorAll('.l-carousel__slides--item'); // スライド要素の取得
+const indicators = document.querySelectorAll('.c-btn__indicator'); // インジケーター要素の取得
 
-showSlide(currentIndex); // 初期スライドを表示→最初にスライドを呼び出す関数を呼んで、最初のスライドを表示
+showSlide(currentIndex); // 初期スライド表示
 
-function showSlide(index) { // 初期スライドを表示する関数
+function showSlide(index) {
     slides.forEach((slide, i) => {
-        slide.style.display = i === index ? 'block' : 'none'; // 各スライドが今のインデックスと一致したら表示('block')、しない非表示('none')
-        indicators[i].classList.toggle('active', i === index); // 各スライドが今のインジケーターと一致したら色変える(::active)
+        slide.style.display = i === index ? 'block' : 'none'; // インデックス一致時に表示
+        indicators[i].classList.toggle('active', i === index); // インジケーターをアクティブに
     });
 }
 
-function nextSlide() { // 次のスライドを表示する関数
-    currentIndex = (currentIndex + 1) % slides.length; // 次のインデックスに進む。スライドのレングス超えたら最初に戻る
-    showSlide(currentIndex); // 新しいスライドの表示
-}
-
-indicators.forEach((indicator, i) => { // インジケーターをクリックしたらスライドを表示する
+indicators.forEach((indicator, i) => { // インジケータークリック時
     indicator.addEventListener('click', () => {
         currentIndex = i;
-        showSlide(currentIndex);
+        showSlide(currentIndex); // 選択したスライドを表示
     });
-});
-
-}
+});}
 
