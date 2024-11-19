559cdd6166561f63896b0d7aa1576e7e12e6e5a3
mihoaota <aota20150707.1020@gmail.com>
2024-11-19 13:51:25 +0900

    Update about.js

--- assets/js/about.js
+++ assets/js/about.js
@@ -27,7 +27,7 @@ export function initAbout() {
             img.alt = item.text;
 
             const captionContainer = document.createElement('div');
-            captionContainer.classList.add('l-about__caption');
+            captionContainer.classList.add('c-text__caption');
 
             const title = document.createElement('p');
             title.textContent = item.text;
