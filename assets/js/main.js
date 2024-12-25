d21d25e93f3136bf34325e7aa5c5887c37dd74bd
‘aota201507071020’ <aota20150707.1020@gmail.com>
2024-12-26 07:33:05 +0900

    12/25

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
 export function initMain(){
     document.addEventListener("DOMContentLoaded", () => {
-        const mainFadeIn = document.querySelector("main.u-fadein");
+    const fadeIns = document.querySelectorAll(".u-fadein");
 
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("show");
-            }
-            });
-        }, { threshold: 0.1 }); // 10%が見えたらトリガー
-
-        observer.observe(mainFadeIn);
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry, index) => {
+        if (entry.isIntersecting) {
+            // 順番に遅延を追加
+            setTimeout(() => {
+            entry.target.classList.add("show");
+            }, index * 200); // 200msずつ遅れさせる
+            observer.unobserve(entry.target);
+        }
         });
+    }, { threshold: 0.1 });
+
+    fadeIns.forEach((fadeIn) => observer.observe(fadeIn));
+    });
+
 };
